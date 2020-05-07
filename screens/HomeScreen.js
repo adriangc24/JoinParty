43285afd1fd4f0e043f5c53a197c1c66cc8718db
adriangc24 <adriangcamacho24@gmail.com>
2020-05-07 22:49:07 +0200

    loads pages and change icon colour

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,45 +1,130 @@
-import React, {Component} from 'react';
-import {View, StyleSheet} from "react-native";
-import * as firebase from 'firebase';
-import { Container, Header, Content, Button, Text } from "native-base";
+import * as React from "react";
+import ProfileScreen from "./ProfileScreen";
+import {
+  Platform,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import * as firebase from "firebase";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Text,
+  FooterTab,
+  Footer,
+  Icon,
+  Left,
+  Body,
+  Title,
+} from "native-base";
+console.disableYellowBox = true;
+
+var props = {
+  activePage: "HomeScreen",
+};
 
 export default class HomeScreen extends React.Component {
-    state = {
-        email: "",
-        displayName: ""
-    }
-
-    componentDidMount(){
-        const {email, displayName} = firebase.auth().currentUser;
-
-        this.setState({email, displayName});
-    };
-
-    signOutUser = () => {
-        firebase.auth().signOut();
-    }
-
-    render() {
-        return (
-            <View id={"homeComps"} style={styles.container}>
-                <Text>Hola {this.state.email}</Text>
-                <View style={{marginTop: "50%"}}>
-                <Button rounded 
-                  style={styles.buttons} 
-                  onPress={this.signOutUser}
-                  >
-                    <Text>Cerrar sesión</Text>
-                  </Button>
-                  </View>
-            </View>
-        );
-    }
+  render() {
+    loadPage();
+    return (
+      <View id={"homeComps"} style={styles.container}>
+        <Content>
+          <View id={"pageToLoad"}>
+            {props.activePage == "ProfileScreen" && <ProfileScreen />}
+          </View>
+        </Content>
+        <View id={"footerContainer"} style={styles.footerContainer}>
+          <Footer style={styles.footer}>
+            <FooterTab style={styles.footerTab}>
+              <Button
+                id={"home"}
+                onPress={() => {
+                  setActivePage("HomeScreen"), this.forceUpdate();
+                }}
+              >
+                <Icon active name="home" style={setIconStyle("HomeScreen")} />
+              </Button>
+              <Button
+                onPress={() => {
+                  setActivePage("AddScreen"), this.forceUpdate();
+                }}
+              >
+                <Icon name="add-circle" style={setIconStyle("AddScreen")} />
+              </Button>
+              <Button
+                id={"search"}
+                onPress={() => {
+                  setActivePage("SearchScreen"), this.forceUpdate();
+                }}
+              >
+                <Icon name="search" style={setIconStyle("SearchScreen")} />
+              </Button>
+              <Button
+                id={"profile"}
+                onPress={() => {
+                  setActivePage("ProfileScreen"), this.forceUpdate();
+                }}
+              >
+                <Icon name="person" style={setIconStyle("ProfileScreen")} />
+              </Button>
+            </FooterTab>
+          </Footer>
+        </View>
+      </View>
+    );
+  }
+}
+function loadPage() {
+  console.log("loading page.....");
+  if (props.activePage == "HomeScreen") {
+    console.log("-- home page");
+  } else if (props.activePage == "SearchScreen") {
+    console.log("-- search page");
+  } else if (props.activePage == "AddScreen") {
+    console.log("-- add page");
+  } else if (props.activePage == "ProfileScreen") {
+    console.log("-- profile page");
+    return <ProfileScreen />;
+  }
 }
 
-const styles = StyleSheet.create ({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
+function setIconStyle(page) {
+  console.log("setting style to icon");
+  if (page == props.activePage) {
+    return {
+      color: "#ffffff",
+    };
+  } else {
+    return {
+      color: "#da0446",
+    };
+  }
+  console.log(this);
+}
+function setActivePage(page) {
+  props.activePage = page;
+  console.log(props.activePage);
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  footerTab: {
+    backgroundColor: "black",
+  },
+});
+
+const iconStyles = StyleSheet.create({
+  icon: {
+    color: "#f7034c",
+  },
+  iconPressed: {
+    color: "#ffffff",
+  },
+});
---
+++ screens/ProfileScreen.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import {
+  Platform,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+} from "react-native";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Text,
+  FooterTab,
+  Footer,
+  Icon,
+  Left,
+  Body,
+  Title,
+} from "native-base";
+
+export default class ProfileScreen extends React.Component {
+  render() {
+    return (
+      <View id={"profileComps"} style={styles.container}>
+        <Content>
+          <Text>PROFILE PAGE</Text>
+        </Content>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  footerTab: {
+    backgroundColor: "black",
+  },
+});
