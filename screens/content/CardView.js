35150f2bba02f708983cffbe338afa862d117158
adriangc24 <adriangcamacho24@gmail.com>
2020-05-08 21:36:17 +0200

    Stable version: trying to play gifs into home screen

---
+++ screens/content/CardView.js
@@ -0,0 +1,107 @@
+import * as React from "react";
+import {
+  Platform,
+  View,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Image,
+  SafeAreaView,
+  ScrollView,
+} from "react-native";
+import Carousel from "react-native-snap-carousel";
+import {
+  Container,
+  Header,
+  Content,
+  Button,
+  Text,
+  FooterTab,
+  Footer,
+  Icon,
+  Left,
+  Body,
+  Title,
+  Item,
+  Input,
+  Card,
+  CardItem,
+  Thumbnail,
+  Right,
+} from "native-base";
+
+export default class CardView extends React.Component {
+  render() {
+    return (
+      <Card style={styles.card}>
+        <CardItem>
+          <Left>
+            <Thumbnail
+              source={{
+                uri:
+                  "https://firebasestorage.googleapis.com/v0/b/joinparty-4e37b.appspot.com/o/adriangc24.jpeg?alt=media&token=cb84c9c2-da81-4c40-8e5f-5ea45c933987",
+              }}
+            />
+            <Body>
+              <Text>Adrián González</Text>
+              <Text note>@adriangc24</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image
+            source={{
+              uri:
+                "https://firebasestorage.googleapis.com/v0/b/joinparty-4e37b.appspot.com/o/posts%2Fparty.gif?alt=media&token=b42c5c17-3e9a-45ce-8966-08bfe6c7792f",
+            }}
+            style={{ height: 200, width: null, flex: 1 }}
+          />
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent>
+              <Icon active name="thumbs-up" />
+              <Text>12</Text>
+            </Button>
+          </Left>
+          <Body>
+            <Button transparent>
+              <Icon active name="chatbubbles" />
+              <Text>4</Text>
+            </Button>
+          </Body>
+          <Right>
+            <Text>11h ago</Text>
+          </Right>
+        </CardItem>
+      </Card>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  carousel: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    marginTop: "45%",
+    transform: [{ rotate: "90deg" }],
+    width: "100%",
+    height: "100%",
+  },
+  container: {
+    flex: 1,
+  },
+  footerTab: {
+    backgroundColor: "black",
+  },
+  card: {
+    width: "90%",
+    //height: "50%",
+  },
+  cardContainer: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    height: "90%",
+  },
+});
--- screens/content/HomeContent.js
+++ screens/content/HomeContent.js
@@ -1,4 +1,6 @@
 import * as React from "react";
+import CardView from "./CardView";
+
 import {
   Platform,
   View,
@@ -7,7 +9,9 @@ import {
   TouchableOpacity,
   Image,
   SafeAreaView,
+  ScrollView,
 } from "react-native";
+import Carousel from "react-native-snap-carousel";
 import {
   Container,
   Header,
@@ -29,52 +33,131 @@ import {
 } from "native-base";
 
 export default class HomeContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: 0,
+      carouselItems: [
+        {
+          title: "Item 1",
+          text: "Text 1",
+        },
+        {
+          title: "Item 2",
+          text: "Text 2",
+        },
+        {
+          title: "Item 3",
+          text: "Text 3",
+        },
+        {
+          title: "Item 4",
+          text: "Text 4",
+        },
+        {
+          title: "Item 5",
+          text: "Text 5",
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <View id={"profileComps"} style={styles.container}>
         <Content>
-          <View id={"cardContainer"} style={styles.cardContainer}>
-            <Card style={styles.card}>
-              <CardItem>
-                <Left>
-                  <Thumbnail source={{ uri: "Image URL" }} />
+          <ScrollView>
+            <View id={"cardContainer"} style={styles.cardContainer}>
+              {/*  <Card style={styles.card}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{ uri: "Image URL" }} />
+                    <Body>
+                      <Text>NativeBase</Text>
+                      <Text note>GeekyAnts</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    source={{ uri: "Image URL" }}
+                    style={{ height: 200, width: null, flex: 1 }}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent>
+                      <Icon active name="thumbs-up" />
+                      <Text>12 Likes</Text>
+                    </Button>
+                  </Left>
                   <Body>
-                    <Text>NativeBase</Text>
-                    <Text note>GeekyAnts</Text>
+                    <Button transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text>4 Comments</Text>
+                    </Button>
                   </Body>
-                </Left>
-              </CardItem>
-              <CardItem cardBody>
-                <Image
-                  source={{ uri: "Image URL" }}
-                  style={{ height: 200, width: null, flex: 1 }}
-                />
-              </CardItem>
-              <CardItem>
-                <Left>
-                  <Button transparent>
-                    <Icon active name="thumbs-up" />
-                    <Text>12 Likes</Text>
-                  </Button>
-                </Left>
-                <Body>
-                  <Button transparent>
-                    <Icon active name="chatbubbles" />
-                    <Text>4 Comments</Text>
-                  </Button>
-                </Body>
-                <Right>
-                  <Text>11h ago</Text>
-                </Right>
-              </CardItem>
-            </Card>
-          </View>
+                  <Right>
+                    <Text>11h ago</Text>
+                  </Right>
+                </CardItem>
+              </Card>
+
+              <Card style={styles.card}>
+                <CardItem>
+                  <Left>
+                    <Thumbnail source={{ uri: "Image URL" }} />
+                    <Body>
+                      <Text>NativeBase</Text>
+                      <Text note>GeekyAnts</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    source={{ uri: "Image URL" }}
+                    style={{ height: 200, width: null, flex: 1 }}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent>
+                      <Icon active name="thumbs-up" />
+                      <Text>12 Likes</Text>
+                    </Button>
+                  </Left>
+                  <Body>
+                    <Button transparent>
+                      <Icon active name="chatbubbles" />
+                      <Text>4 Comments</Text>
+                    </Button>
+                  </Body>
+                  <Right>
+                    <Text>11h ago</Text>
+                  </Right>
+                </CardItem>
+              </Card>*/}
+
+              <CardView />
+              <CardView />
+              <CardView />
+            </View>
+          </ScrollView>
         </Content>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
+  carousel: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    marginTop: "45%",
+    transform: [{ rotate: "90deg" }],
+    width: "100%",
+    height: "100%",
+  },
   container: {
     flex: 1,
   },
@@ -89,5 +172,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flex: 1,
     justifyContent: "center",
+    height: "90%",
   },
 });
