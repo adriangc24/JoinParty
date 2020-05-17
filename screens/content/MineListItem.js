8f8819a32f2cfd6663842d5fe1bdb93e5b2ae96b
adriangc24 <adriangcamacho24@gmail.com>
2020-05-17 23:22:58 +0200

    follows with icon change in real time

--- screens/content/MineListItem.js
+++ screens/content/MineListItem.js
@@ -42,10 +42,12 @@ export const dynamicItem = (
 
       this.handleFollow = this.handleFollow.bind(this);
       this.isFollowing = this.isFollowing.bind(this);
+      this.unfollow = this.unfollow.bind(this);
+      this.follow = this.follow.bind(this);
+      this.checkFollowers = this.checkFollowers.bind(this);
     }
     state = {
       currentUid: null,
-      isFollowing: null,
       followingArray: [],
     };
 
@@ -64,113 +66,148 @@ export const dynamicItem = (
         console.log("UID " + uid);
         console.log("current user " + currentUser);
 
-        var ref = firebase.database().ref("social/" + currentUser + "/follows");
-        ref.on(
-          "value",
-          (snapshot) => {
-            var aux = snapshot.val();
-            let lelaso;
-            for (var propss in aux) {
-              lelaso = propss;
-              try {
-                idUsuario = aux[lelaso].userID;
-                var array = this.state.followingArray;
-                array.push(idUsuario);
-                this.setState({ followingArray: array });
-              } catch (error) {
-                console.log(error);
-              }
-            }
-          },
-          function (errorObject) {
-            console.log("The read failed: " + errorObject.code);
-          }
-        );
+        this.checkFollowers();
       });
     }
 
+    checkFollowers = () => {
+      var currentUser = this.state.currentUid;
+      var array = this.state.followingArray;
+      var ref = firebase.database().ref("social/" + currentUser + "/follows");
+      ref.on(
+        "value",
+        (snapshot) => {
+          var aux = snapshot.val();
+          let lelaso;
+          for (var propss in aux) {
+            lelaso = propss;
+            try {
+              idUsuario = aux[lelaso].userID;
+              this.setState({ followingArray: [] });
+              array.push(idUsuario);
+              this.setState({ followingArray: array });
+            } catch (error) {
+              console.log(error);
+            }
+          }
+          console.log("ARRAY " + this.state.followingArray);
+        },
+        function (errorObject) {
+          console.log("The read failed: " + errorObject.code);
+        }
+      );
+    };
+
     handleFollow = () => {
+      console.log(this.state.followingArray.length);
       let flag = false;
       var currentUser = this.state.currentUid;
-      if (currentUser != uid) {
+      if (this.state.currentUid != uid) {
         for (let i = 0; i < this.state.followingArray.length; i++) {
           if (uid == this.state.followingArray[i]) {
             flag = true;
-            console.log("dentroooooooooooooooooo");
-            var ref = firebase.database().ref("social/" + currentUser);
-
-            ref.child("/follows").on("value", (snapshot) => {
-              console.log(snapshot);
-              //////////////
-              for (var propss in snapshot.val()) {
-                console.log(snapshot.val()[propss]);
-                console.log(snapshot.val()[propss].userID + " UID " + uid);
-                if (snapshot.val()[propss].userID == uid) {
-                  var ref = firebase
-                    .database()
-                    .ref("social/" + currentUser + "/follows");
-                  ref.once("value").then(function (snapshot) {
-                    snapshot.forEach(function (childSnapshot) {
-                      // if(ch){
-
-                      // }
-                      //remove each child
-                      let lol = JSON.stringify(childSnapshot);
-                      console.log(lol);
-                      lol = lol.replace(/"}/, "");
-                      lol = lol.replace(/{"userID":"/, "");
-
-                      console.log("XXXXXXXXXXXXXXXX " + lol);
-                      if (lol == uid) {
-                        console.log("XDD LOL " + childSnapshot.key);
-                        var ref = firebase
-                          .database()
-                          .ref(
-                            "social/" +
-                              currentUser +
-                              "/follows/" +
-                              childSnapshot.key
-                          )
-                          .remove()
-                          .then(() => {
-                            console.log("REMOVE OKKKKKKKKKKKKKKKKKKKKKK");
-                          });
-                      }
-                      console.log("JAJAJJA " + JSON.stringify(childSnapshot));
-                      //console.log(ref.child(childSnapshot));
-                      // ref.child(childSnapshot.key).remove();
-                    });
-                  });
-                }
-                var ref = firebase.database().ref("social/" + currentUser);
-                ref.child("/follows").child(uid);
-              }
-            });
-
-            var ref = firebase.database().ref("social/" + uid);
-
-            ref.child("/followed_by").child(currentUser).remove;
+            this.unfollow();
           }
         }
-        if (!flag) {
-          var ref = firebase
-            .database()
-            .ref("social/" + this.state.currentUid)
-            .child("/follows");
-          ref.push({
-            userID: uid,
-          });
-
-          var ref = firebase
-            .database()
-            .ref("social/" + uid)
-            .child("/followed_by");
-          uid = this.state.currentUid;
-          ref.push({ userID: uid });
-        }
       }
+      if (!flag || this.state.followingArray.length == 0) {
+        this.follow();
+      }
+      this.checkFollowers();
       this.setState(this.state);
       this.forceUpdate();
+    };
+
+    // FOLLOW METHOD
+    follow = () => {
+      console.log("UID " + uid);
+      console.log("UID CURRENT USER" + this.state.currentUid);
+      var currentUser = this.state.currentUid;
+      var ref = firebase
+        .database()
+        .ref("social/" + currentUser)
+        .child("/follows");
+      ref.push({
+        userID: uid,
+      });
+      var ref = firebase
+        .database()
+        .ref("social/" + uid)
+        .child("/followed_by");
+      let ax = this.state.currentUid;
+      ref.push({ userID: ax });
+    };
+
+    // UNFOLLOW METHOD
+    unfollow = () => {
+      //flag = true;
+      var currentUser = this.state.currentUid;
+      // console.log("dentroooooooooooooooooo");
+      // var ref = firebase.database().ref("social/" + currentUser);
+
+      // ref.child("/follows").on("value", (snapshot) => {
+      //   console.log(snapshot);
+      //   for (var propss in snapshot.val()) {
+      //     console.log(snapshot.val()[propss]);
+      //     console.log(snapshot.val()[propss].userID + " UID " + uid);
+      //     if (snapshot.val()[propss].userID == uid) {
+      var ref = firebase.database().ref("social/" + currentUser + "/follows");
+      ref.once("value").then(function (snapshot) {
+        snapshot.forEach(function (childSnapshot) {
+          let lol = JSON.stringify(childSnapshot);
+          console.log(lol);
+          lol = lol.replace(/"}/, "");
+          lol = lol.replace(/{"userID":"/, "");
+
+          console.log("XXXXXXXXXXXXXXXX " + lol);
+          if (lol == uid) {
+            console.log("XDD LOL " + childSnapshot.key);
+            var ref = firebase
+              .database()
+              .ref("social/" + currentUser + "/follows/" + childSnapshot.key)
+              .remove()
+              .then(() => {
+                console.log("REMOVE OKKKKKKKKKKKKKKKKKKKKKK");
+              });
+          }
+        });
+      });
+      //}
+      //  }
+      //});
+      console.log("ADRIAN " + uid);
+      var ref = firebase.database().ref("social/" + uid);
+      ref.child("/followed_by").on("value", (snapshot) => {
+        console.log(snapshot);
+        for (var propss in snapshot.val()) {
+          console.log(snapshot.val()[propss]);
+          console.log(snapshot.val()[propss].userID + " UID " + uid);
+          if (snapshot.val()[propss].userID == uid) {
+            var ref = firebase.database().ref("social/" + uid + "/followed_by");
+            ref.once("value").then(function (snapshot) {
+              snapshot.forEach(function (childSnapshot) {
+                let lol = JSON.stringify(childSnapshot);
+                console.log(lol);
+                lol = lol.replace(/"}/, "");
+                lol = lol.replace(/{"userID":"/, "");
+
+                console.log("XXXXXXXXXXXXXXXXOOOO " + lol);
+                if (lol == uid) {
+                  console.log("XDD LOL " + childSnapshot.key);
+                  var ref = firebase
+                    .database()
+                    .ref("social/" + uid + "/followed_by/" + childSnapshot.key)
+                    .remove()
+                    .then(() => {
+                      console.log("ZZZZ " + uid);
+                      console.log("REMOVE OKKKKKKKKKKKKKKKKKKKKKK");
+                    });
+                }
+              });
+            });
+          }
+        }
+      });
     };
 
     isFollowing = () => {
@@ -184,8 +221,11 @@ export const dynamicItem = (
           />
         );
       }
+      console.log("ARRAY LENGTH " + this.state.followingArray.length);
       for (let i = 0; i < this.state.followingArray.length; i++) {
+        console.log("UID " + uid);
         if (uid == this.state.followingArray[i]) {
+          console.log("FOLLOWEDDDDDDDDDDDDDDDDDDDDDDDDDDD");
           return (
             <Icon
               name={"user-check"}
